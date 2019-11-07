a1b0179daaa6b962c79101c6853041f25aaecc0d
AlanSosaITT <alanguapo99@gmail.com>
2019-11-07 12:47:18 -0800

    Autenticacion

---
+++ Praxtica_04_11_2019_Autenticacion/Praxtica_04_11_2019_Autenticacion/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praxtica_04_11_2019_Autenticacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Register r = new Register();
+            Console.WriteLine("Bienvenido al Login!");
+            int ciclo = 1;
+            while (ciclo == 1)
+            {
+                Console.Write("Registrarse(1), Ingresar(2): ");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        User p = new User();
+                        Console.Write("Ingrese su nombre: ");
+                        p.Name = Console.ReadLine();
+                        Console.Write("Ingrese su nombre de usuario: ");
+                        p.Username = Console.ReadLine();
+                        Console.Write("Ingrese su Contraseña: ");
+                        p.Password = Console.ReadLine();
+                        string ruta = "Datos.txt";
+                        r.Register_user(p, ruta);
+
+                        break;
+                    case "2":
+                        int opcciclo = 1;
+                        while (opcciclo == 1)
+                        {
+
+
+                            Console.Write("Ingrese su nombre de ususario: ");
+                            string nameopc = Console.ReadLine();
+
+                            foreach (var xdxd in r.users)
+                            {
+                                
+                                if (nameopc == xdxd.Username)
+                                {
+                                    int opc = 1;
+                                    while (opc == 1)
+                                    {
+                                        Console.Write("Ingrese su contraseña: ");
+                                        if (Console.ReadLine() == xdxd.Password)
+                                        {
+                                            Console.WriteLine("Bienvenido {0}", xdxd.Name);
+                                            Console.ReadKey();
+                                            opc = 2;
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Incorrecto intente de nuevo");
+                                            
+                                        }
+                                    }
+
+                                    opcciclo = 2;
+
+                                    break;
+                                }
+
+                                
+                            }
+                        }
+                        break;
+                }
+                Console.Write("Desea realizar otra operacion?, Si(1), No(2): ");
+                ciclo = Convert.ToInt32(Console.ReadLine());
+                
+             }Console.ReadKey();
+        }
+    }
+}
---
+++ Praxtica_04_11_2019_Autenticacion/Praxtica_04_11_2019_Autenticacion/Register.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Praxtica_04_11_2019_Autenticacion
+{
+    public class Register
+    {
+
+        public List<User> users = new List<User>();
+        
+        public void Register_user(User user, string Path)
+        {
+            
+            users.Add(user);
+            int desea = 1;
+            Console.Write("Desea ver algun usuario?, Si(1), No(2): ");
+            desea = Convert.ToInt32(Console.ReadLine());
+            if (desea == 1)
+            {
+                Write_user(Path);
+            }                    
+        }
+        public void Write_user(string Path)
+        {
+            Console.Write("Que usuario desea ver?, ingrese su Nombre de usuario: ");
+            string deseado = Console.ReadLine();
+            foreach(var xdxd in users)
+            {
+                if (deseado == xdxd.Username)
+                {
+                    var data = "Nombre: "+xdxd.Name + "\n" +"Contraseña: "+ xdxd.Password + "\n" +"Nombre de usuario: "+ xdxd.Username;
+                    File.WriteAllText(Path, data);
+
+                }
+            }
+        }
+    }
+}
