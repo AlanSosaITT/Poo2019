e95c8b22c928ed7cea04981aa5b3118793eb027d
AlanSosaITT <alanguapo99@gmail.com>
2019-11-07 22:20:42 -0800

    E3_comanda

---
+++ Comanda/Comanda/Aperitivo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comanda
+{
+    class Aperitivo
+    {
+        public int Id { get; set; }
+        //En vez de usar un ; en el get, se usaba una ,
+        public string Nombre { get; set; }
+
+    }
+}
---
+++ Comanda/Comanda/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Comanda
+{
+    public partial class Form1 : Form
+    {
+        readonly BaseDatos menus;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "San Jorge Restaurant";
+            this.BackColor = Color.BurlyWood;
+            menus = new BaseDatos();
+            menuActivo();
+        }
+
+        private void menuActivo()
+        {
+            comboBoxMenu.DataSource = menus.listamenu;
+            comboBoxMenu.DisplayMember = "Nombre";
+            comboBoxMenu.ValueMember = "Id";
+        }
+
+        private void comboBoxMenu_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //El error aqui se encontraba en que al querer pasar una lista al List box, lo que hacia era que le mandaba "menus", que en si es una lista, de listas
+            //Por eso no marcaba el error directamente, pero al correr el programa no tenia coherencia, entonces la solucion fue agregar la lista deseada en cada caso
+            switch (comboBoxMenu.SelectedIndex)
+            {
+                case 1:
+                    listBoxSeleccion.DataSource = menus.ListAperitivos;
+                    listBoxSeleccion.DisplayMember = "Nombre";
+                    listBoxSeleccion.ValueMember = "Id";
+                    break;
+                case 2:
+                    listBoxSeleccion.DataSource = menus.Ensalada;
+                    listBoxSeleccion.DisplayMember = "Nombre";
+                    listBoxSeleccion.ValueMember = "Id";
+                    break;
+                case 3:
+                    listBoxSeleccion.DataSource = menus.ListCarnes;
+                    listBoxSeleccion.DisplayMember = "Nombre";
+                    listBoxSeleccion.ValueMember = "Id";
+                    break;
+                case 4:
+                    listBoxSeleccion.DataSource = menus.ListPescado;
+                    listBoxSeleccion.DisplayMember = "Nombre";
+                    listBoxSeleccion.ValueMember = "Id";
+                    break;
+                case 5:
+                    listBoxSeleccion.DataSource = menus.ListPollo;
+                    listBoxSeleccion.DisplayMember = "Nombre";
+                    listBoxSeleccion.ValueMember = "Id";
+                    break;
+                case 6:
+                    listBoxSeleccion.DataSource = menus.ListPasta;
+                    listBoxSeleccion.DisplayMember = "Nombre";
+                    listBoxSeleccion.ValueMember = "Id";
+                    break;
+                case 7:
+                    listBoxSeleccion.DataSource = menus.ListSandwich;
+                    listBoxSeleccion.DisplayMember = "Nombre";
+                    listBoxSeleccion.ValueMember = "Id";
+                    break;
+                case 8:
+                    listBoxSeleccion.DataSource = menus.ListPaninis;
+                    listBoxSeleccion.DisplayMember = "Nombre";
+                    listBoxSeleccion.ValueMember = "Id";
+                    break;
+                case 9:
+                    listBoxSeleccion.DataSource = menus.ListPostre;
+                    listBoxSeleccion.DisplayMember = "Nombre";
+                    listBoxSeleccion.ValueMember = "Id";
+                    break;
+                case 10:
+                    listBoxSeleccion.DataSource = menus.ListBebida;
+                    listBoxSeleccion.DisplayMember = "Nombre";
+                    listBoxSeleccion.ValueMember = "Id";
+                    break;
+
+                default:
+                    listBoxSeleccion.DataSource = menus.Vacio;
+                    listBoxSeleccion.DisplayMember = "Nombre";
+                    listBoxSeleccion.ValueMember = "ID";
+                   
+
+                    break;
+            }
+            
+           
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            AgregarMenu();   
+        }
+        private void AgregarMenu()
+        {
+            var valor = listBoxSeleccion.SelectedIndex;
+            if (valor!=0)
+            {
+                //Los tipos de datos no coincidian, Debiamos poner el mismo tipo de datos en ambos
+                var datos = Convert.ToDouble(Convert.ToDecimal(comboBoxCantidad.Text) * Convert.ToDecimal(textBoxPrecio.Text));
+                var total = Convert.ToString(datos);
+                 dataGridView1.Rows.Add(listBoxSeleccion.Text, comboBoxCantidad.Text, textBoxPrecio.Text,total);
+                
+
+
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LimpiarAgregar();
+        }
+
+        private void Limpiar()
+        {
+            textBoxPrecio.Text = "";
+            comboBoxCantidad.Text = "0";
+            comboBoxMenu.Text = "None";
+            textBoxTotal.Text = "";
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+
+        }
+
+        private void LimpiarAgregar()
+        {
+            textBoxPrecio.Text = "";
+            comboBoxCantidad.Text = "0";
+            comboBoxMenu.Text = "None";
+            textBoxTotal.Text = "";
+        }
+
+        private void buttonCobrar_Click(object sender, EventArgs e)
+        {
+            cobrar();
+        }
+
+        private void cobrar()
+        {
+            decimal suma = 0;
+            foreach (DataGridViewRow Celda in dataGridView1.Rows)
+            
+                 suma+=Convert.ToDecimal(Celda.Cells["Total"].Value);
+            //Existia un Corchete que estaba sobrado, y nos marcaba un error macivo por consiguiente
+
+            textBoxTotal.Text = Convert.ToString(suma);
+        }
+
+        private void nuevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+          Limpiar();
+        }
+
+        private void cerrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void agregarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AgregarMenu();
+        }
+
+        private void limpiarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LimpiarAgregar();
+        }
+
+        private void cobrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            cobrar();
+        }
+
+        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Proyecto POO Mayo 2019","Acerca de..");
+        }
+
+       
+
+       
+          
+
+
+        
+    }
+}
